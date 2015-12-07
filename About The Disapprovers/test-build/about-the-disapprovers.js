26af45da89b8ea951c6004111c47f450c85dea34
tomekpilot <tomekpilot@gmail.com>
2015-12-07 06:28:53 -0500

    testing build script on About The Disapprovers pg

--- About The Disapprovers/test-build/about-the-disapprovers.js
+++ About The Disapprovers/test-build/about-the-disapprovers.js
@@ -1,7 +1,8 @@
 $(document).ready( function(){
-    $('#disapprovers-menu li a.trigger').click( function(){
+    $('#disapprovers-menu li a.trigger').click( function( e ){
+        e.preventDefault();
         var activeSection = $(  $(this).attr('href'));
-        $('section').not( activeSection).hide();
-        activeSection.show();
+        $('section').not( activeSection).slideUp( "slow" );
+        activeSection.slideDown( "slow" );
     });
 })
