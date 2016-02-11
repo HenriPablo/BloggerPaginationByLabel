e52d66c83cc2d066cc9798ccc6c18b7f91915208
tomekpilot <tomekpilot@gmail.com>
2016-02-11 06:48:33 -0500

    Adding character tooltip

--- story-layout-template.js
+++ story-layout-template.js
@@ -1,18 +1,17 @@
 $(document).ready( function(){
 
     /* DISAPPROVER THUMBNAILS */
-    var molly = { 'src': 'https://4.bp.blogspot.com/--7Fnusj78tg/Vg5g2ee2dcI/AAAAAAAAD0c/h85aEdLkJjE/s200/suzy-and-molly-in-bunndo_1.jpg'};
+    var disapprovers = ['Mr. Toes', 'Molly', 'Abby', 'Cleo', 'Suzy', 'Zeus', 'Frankie', 'Freddie', 'Mila', 'Izzie', 'Appolo', 'Bruno'];
+    var txt = ""
+    
 
-    $('body').append('<div id="disapprover-tip" style="position: absolute; display: none; width: 50px; height: 50px; overflow: hidden; border-radius: 100px;"><img src="" /></div>')
-
-    $('.disapprover-pix-tip').on( 'mouseenter', function(){
-        $('#disapprover-tip img').attr('src', molly );
-        $('#disapprover-tip').show().css({'top': ($(this).offsetTop + 20) + 'px', 'left': ($(this).offsetLeft + 20) + 'px' });
-
+    $(disapprovers).each( function(){
+       txt = $(this).get().join("");
+        console.log(txt)
+        var re = new RegExp(txt, "g");
+        $("div.story-row:contains('"+ txt + "')").html(function(_, html) {
+            return html.replace( re, '<span class="disapprover-pix-tip ' + txt.toLowerCase() + '">' + txt + '</span>');
+});
     });
 
-    $('.disapprover-pix-tip').on( 'mouseleave', function(){
-        $('#disapprover-tip img').attr('src',''  );
-        $('#disapprover-tip').hide();
-    });
 });
