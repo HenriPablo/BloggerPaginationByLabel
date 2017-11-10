2626f7a94f6a4207be4be3ca19e17e9a175fcc21
tomekpilot@gmail.com <nov99344>
2017-11-10 09:42:02 -0500

    incremental re-factorings

--- Pages/About X-Plus Bun/x-plus-bun-episode-list.js
+++ Pages/About X-Plus Bun/x-plus-bun-episode-list.js
@@ -7,21 +7,21 @@
 var xPlusBunEpisodes =
     [
         {
-            "dataDate" : "2017-09-02",
-            "url":"http://www.disapprovingbun.com/2017/09/x-plus-bun-improbabilistic-sim.html",
-            "title" : "X-Plus Bun - The Improbabilistic Sim",
-            "p":"Mr. Toes has a nightmare about flying and landing a space vehicle without being able to stop on a long runway. Him and Abby fly off the end of the runway and fall off a cliff into a an ocean."
+            "d" : "2017-09-02",
+            "u" : "http://www.disapprovingbun.com/2017/09/x-plus-bun-improbabilistic-sim.html",
+            "t" : "X-Plus Bun - The Improbabilistic Sim",
+            "p" : "Mr. Toes has a nightmare about flying and landing a space vehicle without being able to stop on a long runway. Him and Abby fly off the end of the runway and fall off a cliff into a an ocean."
         },
         {
-            "dataDate" : "2017-09-30",
-            "url" : "http://www.disapprovingbun.com/2017/09/x-plus-bun-one-with-void-pt-1-of-3.html",
-            "title" : "One with the void - pt. 1 of 3",
+            "d" : "2017-09-30",
+            "u" : "http://www.disapprovingbun.com/2017/09/x-plus-bun-one-with-void-pt-1-of-3.html",
+            "t" : "One with the void - pt. 1 of 3",
             "p" : "Freddie unveils a hypothesis of what lead up the drifter's demise."
         },
         {
-            "dataDate" : "",
-            "url" : "",
-            "title" : "",
+            "d" : "",
+            "u" : "",
+            "t" : "",
             "p" : ""
         }
     ];
@@ -33,21 +33,21 @@ $( document ).ready( function () {
     for ( var i = 0; i < xPlusBunEpisodes.length; i++ ){
 
         episodesUL.append(
-            $('<li>')
+            $( '<li>' ).attr('data-date', xPlusBunEpisodes[i]["d"] )
                 .append(
-                    $('<a>').attr('href', xPlusBunEpisodes[i]["url"] )
+                    $('<a>').attr('href', xPlusBunEpisodes[i]["u"] )
                         .append(
-                            $('<span>').addClass('date').text( new Date(xPlusBunEpisodes[i]["dataDate"]).toDateString() )
+                            $('<span>').addClass('date').text( new Date(xPlusBunEpisodes[i]["d"]).toDateString() )
                         )
-                        .append(
-                            $('<strong>').addClass('title').text( xPlusBunEpisodes[i]["title"] )
+                         .append(
+                            $('<strong>').addClass('title').text( xPlusBunEpisodes[i]["t"] )
+                            )
                         )
                         .append( $('<p>' )
                             .append(
                                 xPlusBunEpisodes[i]["p"]
                             )
                         )
-            ).data("date", xPlusBunEpisodes[i]["dataDate"] )
         );
 
     }
